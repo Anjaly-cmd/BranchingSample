248b77add8680ba814ef258d0473b056ba34ac3e
Anjaly-cmd <anjaly.devaraj@itcinfotech.com>
2020-05-13 03:22:04 +0530

    new files

---
+++ src/test/java/Demo/Sample/SampleJava.java
@@ -0,0 +1,12 @@
+package Demo.Sample;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sample JAVA!");
+
+	}
+
+}
